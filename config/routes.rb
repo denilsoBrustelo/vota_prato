eb725b6533534c80b2cd03804dbba86409f735cf
Denilso Fabiano Brustelo <denilso.brustelo@gmail.com>
2018-06-25 17:07:15 -0300

    Adicionar arquivos do projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :qualificacoes
+  resources :clientes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    match 'ola' => 'ola_mundo#index', via: 'get'
 
